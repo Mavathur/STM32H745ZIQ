e661ad07756673eb9945b48e26844743f89239a1
mpv3kor <Pruthvi.Mavathur@in.bosch.com>
2023-05-13 02:31:50 +0530

    [TASK-1] Created a cmake based build system to remove the dependence on the IDE.

--- Inc/STM32H745_RCC.h
+++ Inc/STM32H745_RCC.h
@@ -9,9 +9,9 @@
 #define STM32H745_RCC_H_
 #include "STM32H745_ZIQ_HAL_defines.h"
 
-#define RCC_BASE PERIPHERAL_BASE + 0x18024400UL
+#define RCC_BASE PERIPHERAL_BASE + D3_BASE + 0x24400UL
 #define RCC_AHB4ENR *((volatile unsigned int*)(RCC_BASE + RCC_AHB4ENR_OFFSET ))
-
+#define RCC_APB2ENR *((volatile unsigned int*)(RCC_BASE + RCC_APB2ENR_OFFSET ))
 
 
 
